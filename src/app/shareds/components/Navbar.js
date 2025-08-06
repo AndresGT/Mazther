398b9304cf158dec7c1e17d7c512d5a91b5aaaf1
AndresGT <diegogiraldo506@gmail.com>
2025-08-06 14:44:36 -0500

    0.0.2

--- src/app/shareds/components/Navbar.js
+++ src/app/shareds/components/Navbar.js
@@ -1,1 +1,12 @@
-import "@"
+export default function Navbar(){
+  return (
+    <section>
+      <div>
+        <img src="assets/logo.jpg" width={200} height={100}></img>
+        <h2>
+          Mazther Tutoriales
+        </h2>
+      </div>
+    </section>
+  );
+}
